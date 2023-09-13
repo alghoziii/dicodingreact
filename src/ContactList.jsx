8c3d2e40797a916ddf535e6d02e585c0fe104991
shobarudin <shobarudin67@students.amikom.ac.id>
2023-09-14 00:56:35 +0700

    first commit

---
+++ src/ContactList.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ContactItem from "./ContactItem";
+
+// eslint-disable-next-line react/prop-types
+function ContactList({ contacts }) {
+  return (
+    <div className="contact-list">
+      {contacts.map((contact) => (
+        <ContactItem key={contact.id} {...contact} />
+      ))}
+    </div>
+  );
+}
+
+export default ContactList;
